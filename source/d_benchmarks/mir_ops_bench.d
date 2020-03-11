8733b706dd92fb4e7ab8da396b94415a552a928d
p.shkadzko <pavel@gini.net>
2020-03-11 13:41:34 +0100

    updated dub for linux, added L2 norm and sort mir benchmarks

--- source/d_benchmarks/mir_ops_bench.d
+++ source/d_benchmarks/mir_ops_bench.d
@@ -1,6 +1,9 @@
 module mir_ops_bench;
 
 import mir.ndslice;
+import mir.math.sum;
+import mir.math.common : pow, sqrt;
+import mir.ndslice.sorting : sort;
 import mir.random : threadLocalPtr, Random;
 import mir.random.variable : uniformVar, normalVar;
 import mir.random.algorithm : randomSlice;
@@ -13,20 +16,21 @@ import std.stdio;
 
 import mir.math.common : fastmath, optmath;
 
-alias Slice = mir_slice!(double*, 1LU, cast(mir_slice_kind) 2);
+alias SliceArr = mir_slice!(double*, 1LU, cast(mir_slice_kind) 2);
+alias SliceMatrix = Slice!(double*, 2LU, cast(mir_slice_kind) 2);
 
 pragma(inline) static @optmath fmuladd(T, Z)(const T a, Z z)
 {
     return a + z.a * z.b;
 }
 
-double dotProduct(Slice arr0, Slice arr1)
+double dotProduct(SliceArr arr0, SliceArr arr1)
 {
     auto zipped = zip!true(arr0, arr1);
     return reduce!fmuladd(0.0, zipped);
 }
 
-@fastmath double loopedDotProduct(in Slice s0, in Slice s1)
+@fastmath double loopedDotProduct(in SliceArr s0, in SliceArr s1)
 {
     pragma(inline, false);
     double accu = 0;
@@ -38,12 +42,25 @@ double dotProduct(Slice arr0, Slice arr1)
     return accu;
 }
 
+@fastmath double squareL2Norm(SliceMatrix m)
+{
+    pragma(inline, false);
+    auto m0 = m.flattened;
+    double accu = 0;
+    foreach (size_t i; 0 .. m0.length)
+    {
+        accu += m0[i].pow(2);
+    }
+    return accu.sqrt;
+}
+
 /*
 __gshared is a hack that makes value equivalent to a raw C global (module-level) variables.
 It is used here to trigger eager evaluation of the scalar product function which returns single double value.
 */
 __gshared double res2;
 __gshared double res3;
+__gshared double res4;
 
 void reportTime(StopWatch sw, string msg)
 {
@@ -104,13 +121,28 @@ void runMirBenchmarks()
     reportTime(sw, format("Scalar product of 2x%s double slices", rows * cols));
 
     /// Dot product of two double 2D slices.
-    auto m3 = slice!double([rows, cols]);
+    auto m2 = threadLocalPtr!Random.randomSlice(uniformVar!double(-1.0, 1.0), [
+            cols, rows
+            ]);
+    auto m3 = slice!double([rows, rows]);
     sw.reset;
     sw.start;
-    gemm(1.0, m0, m1, 0, m3);
+    gemm(1.0, m0, m2, 0, m3);
     sw.stop;
     reportTime(sw, format("Dot product of two 2D double slices"));
 
     /// L2 norm of double Slice.
-    /// Sort of double Slice.
+    sw.reset;
+    sw.start;
+    auto res4 = squareL2Norm(m1);
+    sw.stop;
+    reportTime(sw, format("L2 norm of %sx%s double slice = %s", rows, cols, res4));
+
+    /// Sort of double Slice along axis=0
+    sw.reset;
+    sw.start;
+    m0.byDim!0
+        .each!sort;
+    sw.stop;
+    reportTime(sw, format("Sorting %sx%s double slice", rows, cols));
 }
