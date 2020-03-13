516893dc8496f7cf95235ec7027296a22a276671
p.shkadzko <pavel@gini.net>
2020-03-13 09:54:25 +0100

    cleaned up a little

--- source/app.d
+++ source/app.d
@@ -6,10 +6,6 @@ import mir_ops_bench;
 int nruns = 20;
 void main()
 {
-    // basic_ops.props;
-    // basic_ops.arrays;
-    // basic_ops.mirArrays;
-    // basic_ops.basicOps;
     runStandardBenchmarks(nruns);
     runMirBenchmarks(nruns);
 }
